fc1b3ac993312633476b355760ee9cf9d837c8e1
Rob-rls <rsummers02@gmail.com>
2016-02-13 00:13:44 +0000

    All menu options now work
    the basic program works!  Next steps are to add checks to make sure
    input is valid every time it is requested.

--- rubyp1.rb
+++ rubyp1.rb
@@ -1,23 +1,31 @@
 #This program should be a simple to do list
 #choices should be add, delete, view, modify, exit
 #Create an array to store to do list
-#The array should be stored in an external file
+#The array should be stored in an external file that is recalled when run again
   
 def option_add
   puts "What is it you would like to add? "
   new_item = $stdin.gets
   end
   
-def option_del
+def option_del #need to add check to make sure option is valid
+  puts "Which item number would you like to delete? "
+  del_item = $stdin.gets.chomp.to_i
+  $to_do_list.slice!(del_item - 1)
 end
   
 def option_mod
+  puts "Which item number would you like to modify? "
+  mod_item_num = $stdin.gets.chomp.to_i
+  add_item = option_add
+  $to_do_list[mod_item_num - 1] = add_item
+  
 end
   
-def option_view #this function has become obsolete as the code works best in the case loop option for view
+def option_view
 	puts "Your current items to do are as follows: "
-	to_do_list.each do |item|
-	  puts "#{to_do_list.index(item)}- #{item}"
+	$to_do_list.each do |item|
+	  puts "#{$to_do_list.index(item) + 1}- #{item}"
 	end
 end
   
@@ -26,17 +34,17 @@ def option_exit
 end
 
 #opens the previous to do list and saves it to an array
-to_do_list = Array.new
+$to_do_list = Array.new
 file_name = "listdata.txt"
 read_file = open(file_name)
-to_do_list = File.readlines(file_name)
+$to_do_list = File.readlines(file_name)
 read_file.close
 #the below puts were for debugging the IO File - remove from final version
-puts to_do_list
-puts to_do_list.class
+puts $to_do_list
+puts $to_do_list.class
 
 quitx = 0
-#get the option form the user
+#get the option form the user; the below menu loops until the exit optin is selected
 until quitx == 1
   
   puts "What would you like to do?"
@@ -48,19 +56,18 @@ until quitx == 1
   
     when "add" 
 	  add_item = option_add
-	  to_do_list.push(add_item)
+	  $to_do_list.push(add_item)
 	  puts "You have added: #{add_item}"
 	  
-    when "delete" then puts "delete"
-	
-	when "modify" then puts "modify"
+    when "delete" 
+	  puts "You have delted: #{option_del}"
+		
+	when "modify" 
+	  puts "The item now reads: #{option_mod}"
     
 	when "view" 
-      puts "Your current items to do are as follows: "
-	  to_do_list.each do |item|
-	    puts "#{to_do_list.index(item) + 1}: #{item}"
-	  end
-    
+      option_view
+	  
 	when "delete" then puts "delete"
 	
     when "exit" 
@@ -72,5 +79,5 @@ until quitx == 1
 end
 
 writefile = open(file_name, "w")
-to_do_list.each {|item| writefile.write(item)}
+$to_do_list.each {|item| writefile.write(item)}
 writefile.close
