d7ea37809756e16189ef54549c9d2947777f61c1
Rob-rls <rsummers02@gmail.com>
2016-02-13 00:36:07 +0000

    The Main file
    this latest edit i've made the delete and modify options also print out
    the list before selecting an item.  The option_add method now takes an
    argument to modify the text prompting the user for imput.

--- rubyp1.rb
+++ rubyp1.rb
@@ -2,22 +2,25 @@
 #choices should be add, delete, view, modify, exit
 #Create an array to store to do list
 #The array should be stored in an external file that is recalled when run again
+#the program requries a file named listdata.txt to be stored in the same directory before it can be run
   
-def option_add
-  puts "What is it you would like to add? "
+def option_add (menu_opt)
+  puts "What would like to #{menu_opt}?"
   new_item = $stdin.gets
   end
   
 def option_del #need to add check to make sure option is valid
+  option_view
   puts "Which item number would you like to delete? "
   del_item = $stdin.gets.chomp.to_i
   $to_do_list.slice!(del_item - 1)
 end
   
 def option_mod
+  option_view
   puts "Which item number would you like to modify? "
   mod_item_num = $stdin.gets.chomp.to_i
-  add_item = option_add
+  add_item = option_add("modify it to")
   $to_do_list[mod_item_num - 1] = add_item
   
 end
@@ -28,10 +31,6 @@ def option_view
 	  puts "#{$to_do_list.index(item) + 1}- #{item}"
 	end
 end
-  
-def option_exit
-  quitx += 1
-end
 
 #opens the previous to do list and saves it to an array
 $to_do_list = Array.new
@@ -39,12 +38,9 @@ file_name = "listdata.txt"
 read_file = open(file_name)
 $to_do_list = File.readlines(file_name)
 read_file.close
-#the below puts were for debugging the IO File - remove from final version
-puts $to_do_list
-puts $to_do_list.class
 
-quitx = 0
 #get the option form the user; the below menu loops until the exit optin is selected
+quitx = 0
 until quitx == 1
   
   puts "What would you like to do?"
@@ -55,7 +51,7 @@ until quitx == 1
   case choice
   
     when "add" 
-	  add_item = option_add
+	  add_item = option_add("add")
 	  $to_do_list.push(add_item)
 	  puts "You have added: #{add_item}"
 	  
@@ -67,17 +63,16 @@ until quitx == 1
     
 	when "view" 
       option_view
-	  
-	when "delete" then puts "delete"
-	
+
     when "exit" 
-	  puts "exiting..."
+	  puts "Saving list and exiting..."
 	  quitx += 1
 	  
     else  puts "I didn't understand, please type a choice as listed."
   end
 end
 
+#saves the to do list to file
 writefile = open(file_name, "w")
 $to_do_list.each {|item| writefile.write(item)}
 writefile.close
